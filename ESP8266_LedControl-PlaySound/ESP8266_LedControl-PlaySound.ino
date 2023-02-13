a3d3b546bfedfe835f2c0ebca2e4d3fdc4106ccb
Muhammet Emre DUŞ <emredus@gmail.com>
2023-02-13 18:08:55 +0300

    ESP8266_LocalIP

---
+++ ESP8266_LedControl-PlaySound/ESP8266_LedControl-PlaySound.ino
@@ -0,0 +1,112 @@
+#include <ESP8266WiFi.h>
+#include "pitches.h"
+
+#define redPin 5      //D1
+#define greenPin 0    //D3
+#define yellowPin 14  //D5
+
+const char* ssid = "Emre";
+const char* password = "93259325";
+
+WiFiServer server(80);
+
+// notes in the melody:
+int melody[] = {
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  4, 8, 8, 4, 4, 4, 4, 4
+};
+
+void setup() {
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(yellowPin, OUTPUT);
+
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  server.begin();
+  Serial.println("Server started");
+
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+  while (!client.available()) {
+    delay(1);
+  }
+  String request = client.readStringUntil('\r');
+  client.flush();
+
+  if (request.indexOf("/red") != -1) {
+    digitalWrite(redPin, HIGH);
+    digitalWrite(greenPin, LOW);
+    digitalWrite(yellowPin, LOW);
+  }
+  if (request.indexOf("/green") != -1) {
+    digitalWrite(redPin, LOW);
+    digitalWrite(greenPin, HIGH);
+    digitalWrite(yellowPin, LOW);
+  }
+  if (request.indexOf("/yellow") != -1) {
+    digitalWrite(redPin, LOW);
+    digitalWrite(greenPin, LOW);
+    digitalWrite(yellowPin, HIGH);
+  }
+  if (request.indexOf("/off") != -1) {
+    digitalWrite(redPin, LOW);
+    digitalWrite(greenPin, LOW);
+    digitalWrite(yellowPin, LOW);
+  }
+
+    // iterate over the notes of the melody:
+  if (request.indexOf("/play") != -1){
+    for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+      // to calculate the note duration, take one second divided by the note type.
+      //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+      int noteDuration = 1000 / noteDurations[thisNote];
+      tone(12, melody[thisNote], noteDuration);
+
+      // to distinguish the notes, set a minimum time between them.
+      // the note's duration + 30% seems to work well:
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+      // stop the tone playing:
+      noTone(12);
+    }
+  }
+
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println("");
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+  client.println("<body>");
+  client.println("LED Control");
+  client.println("<br><br>");
+  client.println("<a href='/red'>Red LED On</a><br>");
+  client.println("<a href='/green'>Green LED On</a><br>");
+  client.println("<a href='/yellow'>Yellow LED On</a><br>");
+  client.println("<a href='/off'>Turn Off All LEDs</a><br>");
+  client.println("<br><br>");
+  client.println("Play sound");
+  client.println("<a href='/play'>Play</a><br>");
+  client.println("</body>");
+  client.println("</html>");
+  delay(1);
+  client.stop();
+}
